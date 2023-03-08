a99eb59c38de130ce9a2514513a579d95cb0fc40
Joaquim Pedroza <71528875+Very-Bad-Goose@users.noreply.github.com>
2023-03-08 14:50:59 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,46 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <chrono>
-#include <cmath>
+#include <thread>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
-
-// Function to generate all possible combinations of characters of a given length
-void generate_combinations(string characters, string prefix, int length)
+bool password_cracker(const std::string& password, const std::string& hash)
 {
-    // If the prefix has the desired length, print it
-    if (prefix.length() == length) {
-        cout << prefix << endl;
-        return;
-    }
-    // Recursively generate combinations with each character of the given set
-    for (int i = 0; i < characters.length(); i++) {
-        string new_prefix = prefix + characters[i];
-        generate_combinations(characters, new_prefix, length);
-    }
-}
-
-// Function to check if a given password matches the target password
-bool check_password(string password, string target)
-{
-    if (password == target) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    // simulate cracking the password hash
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    return password == hash;
 }
 
 int main()
 {
-    // Prompt user to enter the target password
-    string target_password;
-    cout << "Enter the target password: ";
-    cin >> target_password;
+    std::string hash = "5f4dcc3b5aa765d61d8327deb882cf99"; // md5 hash of "password"
 
-    // Define the set of characters that can be used in the password
-    string character_set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}\\|;':\",./<>?`~";
+    std::vector<std::string> passwords = {"password", "123456", "qwerty", "letmein", "monkey",
+                                          "football", "starwars", "iloveyou", "admin", "welcome",
+                                          "hello", "charlie", "123123", "dragon", "baseball"};
 
-    // Define the maximum length of the password to be cracked
-    int max_password_length = 10;
+    std::vector<std::thread> threads;
+    bool found = false;
 
-    // Brute-force the password by trying all possible combinations of characters
-    bool password_found = false;
-    auto start_time = chrono::high_resolution_clock::now();
-    for (int length = 1; length <= max_password_length; length++) {
-        generate_combinations(character_set, "", length);
+    // create and start threads to crack passwords
+    for (const auto& password : passwords)
+    {
+        threads.emplace_back([&found, &password, &hash]() {
+            if (password_cracker(password, hash))
+            {
+                std::cout << "Password found: " << password << std::endl;
+                found = true;
+            }
+        });
     }
-    auto end_time = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time).count();
 
-    cout << "Password cracked in " << duration << " seconds." << endl;
+    // wait for all threads to finish
+    std::for_each(threads.begin(), threads.end(), [](std::thread& t) { t.join(); });
+
+    if (!found)
+    {
+        std::cout << "Password not found" << std::endl;
+    }
 
     return 0;
 }
